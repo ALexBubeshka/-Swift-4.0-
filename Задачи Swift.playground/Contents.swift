5172971e8eb5cb83f52b9530d1e4709e283f0cdb
Alexander <alexanderbubeshka.it@yahoo.com>
2022-05-17 15:20:58 +0300

    1. Константы, переменные и типы данных
    1) Напишите переменные и константы всех базовых типов данных: int, UInt, float, double, string. У чисел вывести их минимальные и максимальные значения.

---
+++ Задачи Swift.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// 1. Константы, переменные и типы данных
+///      1) Напишите переменные и константы всех базовых типов данных: int, UInt, float, double, string. У чисел вывести их минимальные и максимальные значения.
+
+var number: Int = 1
+let nubmerInt: Int = Int.min
+var number1: UInt = UInt.max
+number1 = UInt.min
+var numberInt32: Int32 = Int32.min
+numberInt32 = Int32.max
+var numberInt64: Int64 = Int64.min
+numberInt64 = Int64.max
+var number2: Float = 3.3
+let number3: Double = 3.333
+var number4: String = "333"
+let char: Character = "F"
+
+
+
+
