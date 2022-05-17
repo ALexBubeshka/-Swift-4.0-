9a58cbbcbb5b9f1a61215251011bca8515817bdc
Alexander <alexanderbubeshka.it@yahoo.com>
2022-05-17 16:09:12 +0300

    2. Создайте список товаров

--- Задачи Swift.playground/Contents.swift
+++ Задачи Swift.playground/Contents.swift
@@ -4,19 +4,26 @@ var greeting = "Hello, playground"
 
 // 1. Константы, переменные и типы данных
 ///      1) Напишите переменные и константы всех базовых типов данных: int, UInt, float, double, string. У чисел вывести их минимальные и максимальные значения.
+//
+//var number: Int = 1
+//let nubmerInt: Int = Int.min
+//var number1: UInt = UInt.max
+//number1 = UInt.min
+//var numberInt32: Int32 = Int32.min
+//numberInt32 = Int32.max
+//var numberInt64: Int64 = Int64.min
+//numberInt64 = Int64.max
+//var number2: Float = 3.3
+//let number3: Double = 3.333
+//var number4: String = "333"
+//let char: Character = "F"
 
-var number: Int = 1
-let nubmerInt: Int = Int.min
-var number1: UInt = UInt.max
-number1 = UInt.min
-var numberInt32: Int32 = Int32.min
-numberInt32 = Int32.max
-var numberInt64: Int64 = Int64.min
-numberInt64 = Int64.max
-var number2: Float = 3.3
-let number3: Double = 3.333
-var number4: String = "333"
-let char: Character = "F"
+//2)  Создайте список товаров с различными характеристиками (количество, название). Используйте typealias.
+
+let bread: String = "пол буханки"
+let Pineapple:Int = 2
+var patatoes: String = "2кг"
+var apple = "2 штуки"
 
 
 
