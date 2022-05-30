cd6c09cf24377b1322ccf6dc09f39050fbe2210e
Alexander <alexanderbubeshka.it@yahoo.com>
2022-05-30 15:46:05 +0300

    1.3 и 1.4 выполнил

--- Задачи Swift.playground/Contents.swift
+++ Задачи Swift.playground/Contents.swift
@@ -3,8 +3,8 @@ import UIKit
 var greeting = "Hello, playground"
 
 // 1. Константы, переменные и типы данных
-///      1) Напишите переменные и константы всех базовых типов данных: int, UInt, float, double, string. У чисел вывести их минимальные и максимальные значения.
-//
+// 1) Напишите переменные и константы всех базовых типов данных: int, UInt, float, double, string. У чисел вывести их минимальные и максимальные значения.
+
 //var number: Int = 1
 //let nubmerInt: Int = Int.min
 //var number1: UInt = UInt.max
@@ -20,10 +20,26 @@ var greeting = "Hello, playground"
 
 //2)  Создайте список товаров с различными характеристиками (количество, название). Используйте typealias.
 
-let bread: String = "пол буханки"
-let Pineapple:Int = 2
-var patatoes: String = "2кг"
-var apple = "2 штуки"
+//typealias Slova = String
+//typealias Chisla = Int
+//let bread: Slova = "пол буханки"
+//let Pineapple: Chisla = 2
+//var patatoes: String = "2кг"
+//var apple = "2 штуки"
+
+//3) Напишите различные выражения с приведением типа.
+
+//let welcomeMessage: String = "Hello, World!"
+
+//4) Вычисления с операторами ( умножен ие, деление, сложение, вычитание): создайте консольный калькулятор.
+
+var a = 7
+var b = 2
+let c = a + b
+let d = a / b
+let e = a * b
+a += 2
+b -= 9
 
 
 
